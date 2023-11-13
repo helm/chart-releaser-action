d8d8c9c482288f7beff5928dedc7afbab5f8dca4
Jasper Kamerling <jasper.kamerling@alliander.com>
2023-11-13 14:51:41 +0100

    Use GXF forked chart-releaser
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- cr.sh
+++ cr.sh
@@ -253,7 +253,7 @@ install_chart_releaser() {
     mkdir -p "$install_dir"
 
     echo "Installing chart-releaser on $install_dir..."
-    curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/$version/chart-releaser_${version#v}_linux_amd64.tar.gz"
+    curl -sSLo cr.tar.gz "https://github.com/osgp/chart-releaser/releases/download/$version/chart-releaser_${version#v}_linux_amd64.tar.gz"
     tar -xzf cr.tar.gz -C "$install_dir"
     rm -f cr.tar.gz
   fi
